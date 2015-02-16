96306a0725340fc3e5d2d5e8dc2213ec0c9c6593
Eloy Salinas <eloy@eloysalinas.com>
2015-02-15 17:51:58 -0800

    Login/Register
    Added login and register activities that connect to the UCSC Smartbar
    database to add or check for a user.

--- SmartBar_r1/app/src/main/java/com/example/lamperry/smartbar_r1/LoginActivity.java
+++ SmartBar_r1/app/src/main/java/com/example/lamperry/smartbar_r1/LoginActivity.java
@@ -1,37 +1,86 @@
 package com.example.lamperry.smartbar_r1;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 
 // this class defines all activity within the login screen
 // takes user login/password input and parses through DB to authorize account
 // throws user prompt to try again if account not authorized
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity implements View.OnClickListener {
 
-    EditText usernameText, passwordText;
+    EditText user, pass, usernameText, passwordText; //To store username from login/pass field
+    private Button mSubmit; //Login button
     String usernameString, passwordString;
     ArrayList<String> usernameDB = new ArrayList<>();
     ArrayList<String> passwordDB = new ArrayList<>();
+    // Progress Dialog
+    private ProgressDialog pDialog;
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+
+    //PHPlogin script location:
+    //UCSC Smartbar Server:
+    private static final String LOGIN_URL = "http://www.ucscsmartbar.com/login.php";
+
+    //JSON element ids from repsonse of php script:
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //setup input fields
+        user = (EditText)findViewById(R.id.type_email);
+        pass = (EditText)findViewById(R.id.type_password);
+
+        //setup buttons
+        mSubmit = (Button)findViewById(R.id.login_button);
+
+        //register listeners
+        mSubmit.setOnClickListener(this);
+
         // grabs databases from global class
         usernameDB = ((MyApplication)this.getApplication()).getUsernameDB();
         passwordDB = ((MyApplication)this.getApplication()).getPasswordDB();
+
+
     }
+
+
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.login_button:
+                new AttemptLogin().execute();
+                break;
+
+            default:
+                break;
+        }
+    }
+
 
 
     @Override
@@ -55,6 +104,80 @@ public class LoginActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    class AttemptLogin extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // Check for success tag
+            int success;
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(
+                        LOGIN_URL, "POST", params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Login Successful!", json.toString());
+                    //Intent i = new Intent(Login.this, ReadComments.class);
+                    finish();
+                    //startActivity(i);
+                    return json.getString(TAG_MESSAGE);
+                }else{
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+                Toast.makeText(LoginActivity.this, file_url, Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+    }
+
 
     // parses through database to verify account information
     public void checkLogin(View view) {
--- SmartBar_r1/app/src/main/java/com/example/lamperry/smartbar_r1/NewUserActivity.java
+++ SmartBar_r1/app/src/main/java/com/example/lamperry/smartbar_r1/NewUserActivity.java
@@ -1,27 +1,53 @@
 package com.example.lamperry.smartbar_r1;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.List;
 
 
 // this class defines the behavior of the new user screen
 // namely takes user input to create account, parses through login/password DB to authorize new
 // account, and logs user in
 // throws user prompt if login already created
-public class NewUserActivity extends ActionBarActivity {
+public class NewUserActivity extends ActionBarActivity implements View.OnClickListener {
 
-    EditText newUsernameText, newPasswordText;
+    EditText newUsernameText, newPasswordText, user, pass, agesb, weightsb, sexsb;
     String usernameString, passwordString;
+    private Button mRegister;
     ArrayList<String> usernameDB = new ArrayList<>();
     ArrayList<String> passwordDB = new ArrayList<>();
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+
+    //PHP login script
+
+    //UCSC Smartbar Server:
+    private static final String LOGIN_URL = "http://www.ucscsmartbar.com/register.php";
+
+    //JSON element ids from repsonse of php script:
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +59,24 @@ public class NewUserActivity extends ActionBarActivity {
 
         newUsernameText = (EditText)findViewById(R.id.type_email);
         newPasswordText = (EditText)findViewById(R.id.type_password);
+
+        user = (EditText)findViewById(R.id.type_email);
+        pass = (EditText)findViewById(R.id.type_password);
+        agesb = (EditText)findViewById(R.id.type_age);
+        weightsb = (EditText)findViewById(R.id.type_weight);
+        sexsb = (EditText)findViewById(R.id.type_sex);
+
+        mRegister = (Button)findViewById(R.id.login_button);
+        mRegister.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+
+        new CreateUser().execute();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -85,6 +127,84 @@ public class NewUserActivity extends ActionBarActivity {
             }
         }
         return false;
+    }
+
+
+    class CreateUser extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(NewUserActivity.this);
+            pDialog.setMessage("Creating User...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // Check for success tag
+            int success;
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            String age = agesb.getText().toString();
+            String weight = weightsb.getText().toString();
+            String sex =sexsb.getText().toString();
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+                params.add(new BasicNameValuePair("age", age));
+                params.add(new BasicNameValuePair("weight", weight));
+                params.add(new BasicNameValuePair("sex", sex));
+
+                Log.d("request!", "starting");
+
+                //Posting user data to script
+                JSONObject json = jsonParser.makeHttpRequest(
+                        LOGIN_URL, "POST", params);
+
+                // full json response
+                Log.d("Login attempt", json.toString());
+
+                // json success element
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("User Created!", json.toString());
+                    finish();
+                    return json.getString(TAG_MESSAGE);
+                }else{
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+                Toast.makeText(NewUserActivity.this, file_url, Toast.LENGTH_LONG).show();
+            }
+
+        }
+
     }
 
     // starts welcome activity and sets all relevant global variables
