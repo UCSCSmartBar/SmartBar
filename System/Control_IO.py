01b0b1eeaf184cf831aab54495e4fe09e7ba4f80
masierra <masierra@ucsc.edu>
2015-02-24 22:09:03 +0000

    recommitting this too

--- System/Control_IO.py
+++ System/Control_IO.py
@@ -1,16 +1,21 @@
 #!/usr/bin/
 # author: michael sierra
+import sys
+sys.path.append('/SmartBar/Dispenser/')
+sys.path.append('/SmartBar/System/')
 import RPi.GPIO as GPIO
-import analogspi as spi
+import Finger_Print as FP
+import analogspi as SPI
+import DispensingSystem as DS
+import FingerprintGet as FPQ
 import time
-
+import thread
 
 
 # param: 
 # return: 
 # brief: initializes GPIO, setting appropriate pins.
 def CIO_Initialize():
-    print('GPIO initialized')
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(26, GPIO.OUT)
     GPIO.setup(20, GPIO.OUT)
@@ -18,13 +23,24 @@ def CIO_Initialize():
     GPIO.setup(16,GPIO.OUT) #green
     GPIO.setup(19,GPIO.OUT) #red
     GPIO.setup(21,GPIO.OUT) #blue
-    spi.InitSPI()
+    print('GPIO initialized')
+    SPI.InitSPI()
+    print('SPI initialized')
+    FPQ.fpget_init()
+    print('FPFS initialized')
+
+# param: 
+# return: 
+# brief: closes GPIO   
 def CIO_Free():
     print 'goodbye -love gpio'
     GPIO.cleanup()
-    
+
+# param: string: string to be parsed
+# return: string to be sent back to UI
+# brief: Parses the string and calls appropriate function.
 def Parse_Message(string):
-    if not hasattr(Parse_Message, "led0Val"):
+    if not hasattr(Parse_Message, "led0Val"):   #toggle values
         Parse_Message.led0Val = 0
     if not hasattr(Parse_Message, "led1Val"):
         Parse_Message.led1Val = 0
@@ -39,30 +55,32 @@ def Parse_Message(string):
     elif string == '$LED':
         Parse_Message.LED_Val = not Parse_Message.LED_Val
     elif string == '$Error':
-        return 'Error'
+        return '$ACK|Error'
     elif string == '$Warn':
-        return 'Warning'
-    else:
+        return '$ACK|Warning'
+    else:                                       #strings to parse
         print string
-        sList = string.split(".")
-        print sList
-        if(sList[0] == "$Valve"):
-            try:
-                print sList[1]
-                T = int(sList[1])
-                print 'T is ' + sList[1]
-            except:
-                print 'undef'
-                return
-            print 'back on'
-            GPIO.output(20, 1)
-            GPIO.output(26, 1)
-            #Turns on LED for variable amount of time
-            time.sleep(T)
-            GPIO.output(20, 0)
-            GPIO.output(26, 0)
-        elif (sList[0] == '$DO') :
-            print sListmcp[1]
-        elif (sList[0] == '$AD'):
-            ADstr = '$AD.' + str(spi.ReadChannel(0))            
-            return ADstr
+        sList = string.split(",")
+        #print sList
+        if (sList[0] == '$DO') :
+            Dispenser = DS.SmartBar_Dispenser()
+            Dispenser.ReceiveCommand(string)
+            print('hi mom')
+            del Dispenser
+            return '$ACK|DO'
+        elif (sList[0] == '$AD'):               #returns ADC value
+            ADstr = '$AD.' + str(spi.ReadChannel(sList[1]))            
+            return '$ACK|AD'
+        elif (sList[0] == '$FP'):
+            FP.Finger_PrintSM(sList[1])
+            return '$ACK|FP'
+        elif (sList[0] == '$FPQ'):
+            FPQ.get_files(string)
+            return '$ACK|FPQ'
+        else:
+            return '$NAK'
+        
+            
+            
+
+        
