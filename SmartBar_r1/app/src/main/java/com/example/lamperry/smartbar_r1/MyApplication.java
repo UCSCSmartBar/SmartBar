5c471a2a20c4c24b1cea234b6ba9acca5711ffe7
lauren <lamperry@ucsc.edu>
2015-02-24 19:23:32 -0800

    Coo Shit
    library on top

--- SmartBar_r1/app/src/main/java/com/example/lamperry/smartbar_r1/MyApplication.java
+++ SmartBar_r1/app/src/main/java/com/example/lamperry/smartbar_r1/MyApplication.java
@@ -2,6 +2,8 @@ package com.example.lamperry.smartbar_r1;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.view.inputmethod.InputMethodManager;
 
 /*
@@ -15,10 +17,25 @@ public class MyApplication extends Application {
 
     public void setLoggedIn(boolean setLogIn) {
         loggedIn = setLogIn;
+        myPin = getMy10DigitPhoneNumber();
+        for (int i = myPin.length(); i < 11; i++) {
+            myPin = "1" + myPin;
+        }
     }
 
     public static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager)activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    private String getMyPhoneNumber() {
+        TelephonyManager mTelephonyMgr;
+        mTelephonyMgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        return mTelephonyMgr.getLine1Number();
+    }
+
+    private String getMy10DigitPhoneNumber() {
+        String s = getMyPhoneNumber();
+        return s.substring(2);
     }
 }
